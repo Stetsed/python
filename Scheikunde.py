b4d488b7c2c4b91b2e8138e48adb04583eacb5b1
Stetsed <stetsed@protonmail.com>
2022-02-01 18:21:51 +0100

    Calculate_Hoeveelheid_Mol

--- Scheikunde.py
+++ Scheikunde.py
@@ -13,4 +13,20 @@ def calculate_moleculaire_massa():
     global answer
     answer = (str(moleculaire_massa) + 'g/mol')
 
+def calculate_hoeveelheid_mol():
+    stoff = input("Welke Stof?: ")
+    gewicht = input("Hoeveel stof in g?: ")
+
+    f = Formula(stoff)
+    moleculaire_massa = f.mass
+
+    hoeveelheid_mol = ( float(gewicht) / float(moleculaire_massa) )
+
+    print (hoeveelheid_mol)
+
+    global answer
+    answer = (str(hoeveelheid_mol) + 'mol')
+
+
+
 
