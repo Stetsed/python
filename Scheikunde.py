3809f7fc7bebcd1b9d8bfc908cf5cdf4b282dc3e
Stetsed <stetsed@protonmail.com>
2022-02-06 16:16:13 +0100

    Added Calculate_hoeveelheid_ml

--- Scheikunde.py
+++ Scheikunde.py
@@ -24,18 +24,32 @@ def calculate_hoeveelheid_mol():
 
     hoeveelheid_mol = ( float(gewicht) / float(moleculaire_massa) )
 
-    print (hoeveelheid_mol)
-
     global answer
-    answer = (str(hoeveelheid_mol) + 'mol')
+    answer = (str(hoeveelheid_mol) + ' mol')
 
     selection_menu()
 
+def calculate_hoeveelheid_ml():
+    stoff = input("Welke Stof?: ")
+    gewicht = input("Hoeveel stof in g?: ")
+    concentratie = input("Hoeveel mol/L zijn de oplossingen?: ")
+
+    f = Formula(stoff)
+    moleculaire_massa = f.mass
+
+    hoeveelheid_mol = ( float(gewicht) / float(moleculaire_massa) )
+
+    hoeveelheid_ml = ( float(hoeveelheid_mol) / float(concentratie) * 1000)
+
+    global answer
+    answer = (str(hoeveelheid_ml) + ' ml')
+
+    selection_menu()
 
 def selection_menu():
     # Just for choosing the function
     print("\033c")
-    print("Please pick one of the following options \n 1. Calculate Moleculaire Massa. \n 2. Calculate hoeveelheid mol. \n 3. Exit \n Current Answer: " + str(answer))
+    print("Please pick one of the following options \n 1. Calculate Moleculaire Massa. \n 2. Calculate hoeveelheid mol.\n 3. Calculate hoeveelheid ml \n 4. Exit \n Current Answer: " + str(answer))
     selection = input(" Selection: ")
     if selection == "1":
         print("\033c")
@@ -44,4 +58,7 @@ def selection_menu():
         print("\033c")
         calculate_hoeveelheid_mol()
     elif selection == "3":
+        print("\033c")
+        calculate_hoeveelheid_ml()
+    elif selection == "4":
         exit()
